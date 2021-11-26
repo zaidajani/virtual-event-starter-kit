4a67f2e81876c066b11492b7512af361cfa55e85
Solomon lijo <71135230+solomonshalom@users.noreply.github.com>
2021-11-26 11:29:49 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,19 +14,19 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'https://vercel.com/solomonshalom/virtual-event-starter-kit/EeLMVcAjuPcKatm9dFy3uNkxuQVz';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = '@21Kschool';
+export const BRAND_NAME = '21K School';
+export const SITE_NAME_MULTILINE = ['21K', 'School'];
+export const SITE_NAME = '21K School';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'Social Fest is the next big thing #21kschool #SocialFest';
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
-export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+  'An interactive online experience by the students, free for everyone.';
+export const DATE = 'December 30, 2020';
+export const SHORT_DATE = 'Dec 30 - 9:00am PST';
+export const FULL_DATE = 'Dec 30th 9am Pacific Time (GMT-7)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
@@ -36,8 +36,8 @@ export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
 export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
 export const CODE_OF_CONDUCT =
-  'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
-export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
+  'https://github.com/solomonshalom/virtual-event-starter-kit/blob/main/CODE_OF_CONDUCT.md';
+export const REPO = 'https://github.com/solomonshalom/virtual-event-starter-kit';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
